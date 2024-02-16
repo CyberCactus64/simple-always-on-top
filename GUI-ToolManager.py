de4b436a73b31c38f5c9fb6d0359098fbd1d424e
CyberCactus64 <edoardo.enricomaria.fornasier@gmail.com>
2024-02-16 15:22:24 +0100

    A starting point...

--- GUI-ToolManager.py
+++ GUI-ToolManager.py
@@ -6,23 +6,20 @@ import subprocess
 import ctypes
 
 
-icon_path = os.path.join("icons", "icon.png") # /folder
+icon_path = os.path.join("graphics", "icon.png")
 icon_image = Image.open(icon_path)
 icon = setIcon("SimpleAlwaysOnTop", icon_image, "Simple Always On Top") # generate the icon
 
 def run_management_app():
-    subprocess.run(["path_to_your_management_app.exe"])
+    subprocess.run(["path_to_the_management_app.exe"])
 
-# Definizione delle azioni del menu contestuale
 menu = (item("Open Management App", run_management_app),
         item("Exit", lambda: icon.stop()))
 
-# Associazione del menu contestuale all'icona della traybar
 icon.menu = menu
 
-# Nascondi l'icona dell'applicazione dalla barra delle applicazioni
+# hide the app from the applications bar
 ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
 
-# Avvio dell'applicazione
 icon.run()
 
