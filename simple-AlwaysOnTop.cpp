32779edfe2828322ec55b3c73454640afa00b39e
CyberCactus64 <edoardo.enricomaria.fornasier@gmail.com>
2024-02-28 22:11:58 +0100

    Update the main code, now run in a thread

--- simple-AlwaysOnTop.cpp
+++ simple-AlwaysOnTop.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <Windows.h>
+#include <iostream>
 
 
 void SetWindowAlwaysOnTop(HWND hwnd) {
@@ -14,12 +14,12 @@ HWND GetActiveWindowHandle() {
     return GetForegroundWindow();
 }
 
-
-int main() {
+// background function of the thread
+DWORD WINAPI BackgroundThread(LPVOID lpParam) {
     bool alwaysOnTopActivated = false;
-    std::cout << "HOW TO USE:\nENABLE Always On Top mode --> WIN + SHIFT + T\nDISABLE Always On Top mode --> WIN + SHIFT + Y" << std::endl; 
 
     while (true) {
+        // background code:
         if ((GetAsyncKeyState(VK_LWIN) & 0x8000) && (GetAsyncKeyState(VK_SHIFT) & 0x8000) && (GetAsyncKeyState('T') & 0x8000)) {
             HWND hwnd = GetActiveWindowHandle();
             if (hwnd != NULL) {
@@ -41,10 +41,29 @@ int main() {
                 std::cout << "Unable to get active window handle." << std::endl;
             }
         }
-        
 
         Sleep(100);
     }
+
+    return 0;
+}
+
+int main() {
+    HANDLE hThread; // manage the thread
+
+    // create the thread in background (call the function BackgroundThread())
+    hThread = CreateThread(NULL, 0, BackgroundThread, NULL, 0, NULL);
+    if (hThread == NULL) {
+        std::cerr << "Error...." << GetLastError() << std::endl;
+        return 1;
+    }
+    std::cout << "HOW TO USE:\nENABLE Always On Top mode --> WIN + SHIFT + T\nDISABLE Always On Top mode --> WIN + SHIFT + Y\n" << std::endl;
+
+    // wait while thread is running
+    WaitForSingleObject(hThread, INFINITE);
+
+    // close the thread
+    CloseHandle(hThread);
 
     return 0;
 }
